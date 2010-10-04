83221877d45146b54a98475615c417d4f9fee217
stdbrouw <stijn.debrouwere@gmail.com>
2010-10-04 10:41:25 +0200

    Cleanup.

--- core-packages/http/lib/auth.jsx
+++ core-packages/http/lib/auth.jsx
@@ -1,58 +1,42 @@
-﻿// pre-Extendables code
-// - needs a big cleanup
-// - needs to become generic rather than Drupal-bound
-// - should comprise both an auth part, and a UI part
+﻿exports.AuthenticationDialog = AuthenticationDialog;
 
-/*
-	> http://en.wikipedia.org/wiki/Basic_access_authentication#Example
-	> http://en.wikipedia.org/wiki/Digest_access_authentication#Example_with_explanation	
-*/
+var ui = require("ui");
 
-function login_drupal() {
-var cancellation = false;
-var response = "";
+function AuthenticationDialog (request) {
+	var mixins = {
+		'masked': {}
+	}
+	// 1. dialog
+	var dialog = this.dialog = new ui.Dialog().with(mixins);
+	dialog.row('username').text('label', 'Username').input('value', '').using('masked');
+	dialog.row('password').text('label', 'Username').input('value', '').using('masked');	
+	dialog.row('confirmation').text('status', '').button('cancel', 'Cancel').button('ok', 'Authenticate');
 
-/* LOGINFUNCTIONALITEIT */
-var myDialog = new Window('dialog', 'Authentication with Drupal', null, {orientation: 'column'});
-var myLogin = myDialog.add('group', null, null, {orientation: 'row'});
-var mySubmit = myDialog.add('group', null, null, {orientation: 'row', alignment: 'right'});
+	dialog.confirmation.cancel.on('click').do(function () {
+		this.window.close();
+	});
+
+	dialog.confirmation.ok.on('click').do(function () {
+		request.auth.basic(username, password);
+		this.window.close();
+	});
+
+	dialog.confirmation.status.update = function (message) {
+		this.dialog.confirmation.status.text = message;
+	}
+
+	this.do = function () {
+		this.dialog.show();
+		return request.do();
+	}
 	
-myLogin.userLabel = myLogin.add('statictext', undefined, 'Username');
-myLogin.userField = myLogin.add('edittext', undefined, '');
-	myLogin.userField.characters = 20;
-myLogin.passLabel = myLogin.add('statictext', undefined, 'Password');
-myLogin.passField = myLogin.add('edittext', undefined, '', {noecho: true});
-	myLogin.passField.characters = 20;
-
-mySubmit.status = mySubmit.add('statictext', [undefined, undefined, 250, 20], '');
-mySubmit.submit = mySubmit.add('button', undefined, 'Authenticate');
-mySubmit.cancel = mySubmit.add('button', undefined, 'Cancel');
-
-mySubmit.submit.onClick = function() {
-  // Als gesubmit wordt
-  dialogUsername = myLogin.userField.text;
-  dialogPassword = myLogin.passField.text;
-  response = dropbox(dialogUsername, dialogPassword, "");
-  this.parent.parent.close(1);
+	
 }
 
-mySubmit.cancel.onClick = function() {
-  this.parent.parent.close(1);
-  cancellation = true;
+// usage example: 
+
+while (response.status == 401) {
+	var response = new AuthenticationDialog(request).do();
+	// retry login
+	dialog.confirmation.status.update("Authentication failed. Please try again.");
 }
-
-// see if we haven't already entered our username & pass
-response = dropbox(dialogUsername, dialogPassword, "");
-
-while (response != "T" && cancellation == false) {
-		// TODO: onderscheid kunnen maken tussen rejectie en gefaalde connectie
-		myDialog.show();
-		if (response == "F") {
-		mySubmit.status.text = "Authentication failed. Please try again.";
-		} else {
-			mySubmit.status.text = "Can't connect to the Drupal server.";
-			}
-}
-
-return response;
-} // f:login_drupal
