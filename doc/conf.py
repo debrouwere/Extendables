4820b870e23800f3ae6f78f18b0e5f533158b9df
stdbrouw <stijn.debrouwere@gmail.com>
2010-11-01 14:58:11 +0100

    Added google analytics to the docs

--- doc/conf.py
+++ doc/conf.py
@@ -25,7 +25,8 @@ sys.path.insert(0, os.path.abspath('./_ext'))
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['glob_include']
+extensions = ['glob_include', 'sphinxcontrib.googleanalytics']
+googleanalytics_id = 'UA-19440923-1'
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
